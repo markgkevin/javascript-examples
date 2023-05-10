e748225596df542535e740b924c5c6e209b7149b
markgkevin <markymend1001@gmail.com>
2023-05-11 05:04:24 +0800

    js functions

---
+++ function-structure.js
@@ -0,0 +1,25 @@
+// creating a javascript functions
+
+
+//functions without parameter
+
+function addTwoNum() { // function name
+    var num1 = 10;
+    var num2 = 25;     
+    var result = num1 + num2;
+
+    console.log(result);
+}
+addTwoNum(); // calling function/invoke
+
+//-------------------------------------------------//
+
+// function with parameter
+
+function sumOfTwoNum(a, b) {
+    var sumResult = a + b;
+
+    console.log(sumResult);
+
+}
+sumOfTwoNum(10,15); //invoke/calling function with arguments
