1940f5de8d9495270d4cb635a169a27f0eccdfa3
markgkevin <markymend1001@gmail.com>
2023-05-16 14:54:57 +0800

    end of week two

---
+++ error-handling/commonErrors.js
@@ -0,0 +1,13 @@
+// Common Errors in Javascript
+
+
+// syntax error
+// var name = "mark;
+// console.log(name); // SyntaxError: Invalid or unexpected token
+
+// reference error
+// console.log(a + b); // ReferenceError: a is not defined
+
+// type error
+// var word = "Hello";
+// word.pop(); //TypeError: word.pop is not a function
---
+++ js-operator/typeOf.js
@@ -0,0 +1,18 @@
+// using type of operator
+// Javascript operator that evaluates parameter and returns the data types as a string
+
+
+// var test = typeof ('what is  this'); //string
+// var test = typeof (110); // number
+// var test = typeof (3.14); // number
+// var test = typeof (false); // boolean
+// var test = typeof (1 < 2); // boolean
+// var test = typeof ([1, 2, 3]); // object  // cause array is an obbject
+// var test = typeof ({ firstProperty: 1 }); // object // object literal key:value pair
+// var test = typeof (function abc() { console.log('abc') }); //function
+
+console.log(test);
+
+
+
+
