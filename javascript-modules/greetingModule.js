2199bf1c8a44cf0376c8ff3c4fd947e3479c7189
markgkevin <markymend1001@gmail.com>
2023-05-29 19:05:23 +0800

    DOM

---
+++ javascript-modules/greetingModule.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hello ${person}`);
+}
+
+export function formalGreeting(person) {
+    console.log(`Good day ${person}`);
+}
+
+const greeting = () => {
+    console.log('Howdy!');
+}
+
+export default greeting;
